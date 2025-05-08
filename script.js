c52e8a3d447515de8a16f599b42a992e362e2f0a
Bongani Mbambiso <mbambisob@gmail.com>
2025-05-08 06:57:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     const hamburgerButton = document.getElementById('hamburger-button');
     const mobileMenu = document.getElementById('mobile-menu');
-    const hamburgerIcon = document.querySelector('.hamburger-menu'); // Changed from .hamburger-icon
 
     hamburgerButton.addEventListener('click', function() {
         mobileMenu.classList.toggle('open');
-        hamburgerIcon.classList.toggle('open');
+        hamburgerButton.classList.toggle('open');
     });
 });
